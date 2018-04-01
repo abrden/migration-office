991eeb98bed02f7fe9b5a8d84cda0c29ef7d8e32
Manuel Porto <manu.porto94@gmail.com>
2018-04-01 01:45:51 -0300

    Extract features from file

--- src/main/PersonsGenerator.cpp
+++ src/main/PersonsGenerator.cpp
@@ -20,17 +20,19 @@ std::vector<std::string> get_next_line_and_split_into_tokens(std::istream &str)
         result.push_back(cell);
     }
     // This checks for a trailing comma with no data after it.
-    if (!lineStream && cell.empty())
-    {
-        // If there was a trailing comma then add an empty element.
-        result.push_back("");
-    }
+//    if (!lineStream && cell.empty())
+//    {
+//        // If there was a trailing comma then add an empty element.
+//        result.push_back("");
+//    }
     return result;
 }
 
-//std::list<Feature*> extract_features(std::vector<std::string> raw_features) {
-//    return std::list<Feature*> l;
-//}
+std::list<Feature*> extract_features(std::vector<std::string> raw_features) {
+    std::list<Feature*> l;
+    for(auto const& i : raw_features) l.push_back(new Feature(i));
+    return l;
+}
 
 PersonsGenerator::PersonsGenerator(const std::string& persons_file_path) {
     std::ifstream ifs(persons_file_path);
@@ -39,16 +41,15 @@ PersonsGenerator::PersonsGenerator(const std::string& persons_file_path) {
     while (getline(ifs, line)) {
 
         std::vector<std::string> tokenized_line = get_next_line_and_split_into_tokens(ifs);
-        std::list<Feature*> person_features;
 
         // TODO don't use magic numbers, define them in a enum
 //        int timestamp = stoi(tokenized_line[0]);
-        bool resident = stoi(tokenized_line[1]) == 1 ? true : false;
-        int id = stoi(tokenized_line[2]);
+        bool resident = stoi(tokenized_line[1]) == 1;
+        auto id = (unsigned int) stoi(tokenized_line[2]);
         std::string name = tokenized_line[3];
         std::string last_name = tokenized_line[4];
         std::vector<std::string> raw_features(tokenized_line.begin() + 5, tokenized_line.end());
-//        person_features = extract_features(raw_features);
+        std::list<Feature*> person_features = extract_features(raw_features);
 
         if (resident) {
             persons.push(new Resident(id, person_features));
