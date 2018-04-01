15ebc196f8ef1a9d9a5f41620d712f97e91e01a8
Manuel Porto <manu.porto94@gmail.com>
2018-04-01 18:30:19 -0300

    Formatting

--- src/main/PersonsGenerator.cpp
+++ src/main/PersonsGenerator.cpp
@@ -27,13 +27,13 @@ std::vector<std::string> split_line_into_tokens(std::string &line) {
     return result;
 }
 
-std::list<Feature *> extract_features(std::vector<std::string> raw_features) {
-    std::list<Feature *> l;
+std::list<Feature*> extract_features(std::vector<std::string> raw_features) {
+    std::list<Feature*> l;
     for (auto const &i : raw_features) l.push_back(new Feature(i));
     return l;
 }
 
-PersonsGenerator::PersonsGenerator(const std::string &persons_file_path) {
+PersonsGenerator::PersonsGenerator(const std::string& persons_file_path) {
     std::ifstream ifs(persons_file_path);
     std::string line;
 
@@ -44,11 +44,11 @@ PersonsGenerator::PersonsGenerator(const std::string &persons_file_path) {
 
         int timestamp = stoi(tokenized_line[Fields::timestamp]);
         bool resident = stoi(tokenized_line[Fields::resident]) == 1;
-        auto id = (unsigned int) stoi(tokenized_line[Fields::id]);
+        unsigned int id = (unsigned int) stoi(tokenized_line[Fields::id]);
         std::string name = tokenized_line[Fields::name];
         std::string last_name = tokenized_line[Fields::last_name];
         std::vector<std::string> raw_features(tokenized_line.begin() + Fields::features_start, tokenized_line.end());
-        std::list<Feature *> person_features = extract_features(raw_features);
+        std::list<Feature*> person_features = extract_features(raw_features);
 
         if (resident) {
             persons.push(std::make_pair(timestamp, new Resident(id, person_features)));
@@ -56,10 +56,9 @@ PersonsGenerator::PersonsGenerator(const std::string &persons_file_path) {
             persons.push(std::make_pair(timestamp, new Foreigner(Passport(), person_features)));
         }
     }
-    // TODO sort persons array
 }
 
-const std::queue<std::pair<unsigned int, Person *>> &PersonsGenerator::get_persons() {
+const std::queue<std::pair<unsigned int, Person*>>& PersonsGenerator::get_persons() {
     return persons;
 }
 
