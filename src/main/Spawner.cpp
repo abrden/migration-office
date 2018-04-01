fdb6c9c5b68ca7cae7d71c440538a920d90797c7
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-04-01 20:30:15 -0300

    Fix linker error

--- src/main/Spawner.cpp
+++ src/main/Spawner.cpp
@@ -1,4 +1,7 @@
 #include "Spawner.h"
+#include "WantedPersonAlert.h"
+
+template class Spawner<WantedPersonAlert>;
 
 template <class T>
 Spawner<T>::Spawner(std::list<std::pair<int, T*>>& items) : items(items) {
