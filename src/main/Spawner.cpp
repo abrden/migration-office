57589610bf447063c397af3cffd73dfed0a132c7
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-04-01 23:51:58 -0300

    Add spawner run

--- src/main/Spawner.cpp
+++ src/main/Spawner.cpp
@@ -1,6 +1,9 @@
 #include "Spawner.h"
 #include "WantedPersonAlert.h"
 
+#include<ctime>
+#include <iostream>
+
 template class Spawner<WantedPersonAlert>;
 
 template <class T>
@@ -10,7 +13,20 @@ Spawner<T>::Spawner(std::list<std::pair<int, T*>>& items) : items(items) {
 
 template <class T>
 void Spawner<T>::run() {
-    // TODO
+    std::time_t t0 = std::time(0);
+
+    while (!items.empty()) {
+        std::time_t t = std::time(0);
+
+        for (typename std::list<std::pair<int, T*>>::const_iterator iterator = items.begin(), end = items.end();
+             iterator != end && (*iterator).first <= (t - t0);
+             ++iterator) {
+            // TODO Spawn the item!
+            std::cout << "Time passed: " << (t - t0) << " - Spawning item with timestamp " << (*iterator).first << std::endl;
+            delete (*iterator).second; // FIXME temporary deletion to avoid mem leak
+            iterator = items.erase(iterator);
+        }
+    }
 }
 
 template <class T>
--- src/test/test_spawner.cpp
+++ src/test/test_spawner.cpp
@@ -3,10 +3,17 @@
 #include "catch.hpp"
 #include <list>
 
-TEST_CASE("Spawner", "[feature]") {
+TEST_CASE("Spawner sort") {
     MinisterOfSecurity m("../resources/alerts.txt");
     std::list<std::pair<int, WantedPersonAlert*>>& wpa = m.get_alerts();
     Spawner<WantedPersonAlert> s(wpa);
     REQUIRE(wpa.front().first == 0);
     REQUIRE(wpa.back().first == 4);
+}
+
+TEST_CASE("Spawner run") {
+    MinisterOfSecurity m("../resources/alerts.txt");
+    std::list<std::pair<int, WantedPersonAlert*>>& wpa = m.get_alerts();
+    Spawner<WantedPersonAlert> s(wpa);
+    s.run();
 }
