6be5d1d63cf5340a53dc57664169dc13ae8056df
Manuel Porto <manu.porto94@gmail.com>
2018-04-02 17:49:27 -0300

    Add tests for PersonsGenerator and Spawner (failing)

--- src/test/test_spawner.cpp
+++ src/test/test_spawner.cpp
@@ -2,18 +2,44 @@
 #include "MinisterOfSecurity.h"
 #include "catch.hpp"
 #include <list>
+#include <src/main/PersonsGenerator.h>
 
 TEST_CASE("Spawner sort") {
-    MinisterOfSecurity m("../resources/alerts.txt");
-    Spawnables& a = m.get_alerts();
-    Spawner s(a);
-    REQUIRE(a.get_items().front().first == 0);
-    REQUIRE(a.get_items().back().first == 4);
+
+    SECTION("security alerts") {
+        MinisterOfSecurity m("../resources/alerts.txt");
+        Spawnables& a = m.get_alerts();
+        Spawner s(a);
+
+        REQUIRE(a.get_items().front().first == 0);
+        REQUIRE(a.get_items().back().first == 4);
+    }
+
+    SECTION("persons") {
+        PersonsGenerator pg("../resources/persons.txt");
+        Spawnables& p = pg.get_persons();
+        Spawner s(p);
+
+        REQUIRE(p.get_items().front().first == 0);
+        REQUIRE(p.get_items().back().first == 1);
+    }
 }
 
 TEST_CASE("Spawner run") {
-    MinisterOfSecurity m("../resources/alerts.txt");
-    Spawnables& a = m.get_alerts();
-    Spawner s(a);
-    s.run();
+
+    SECTION("with security alerts") {
+        MinisterOfSecurity m("../resources/alerts.txt");
+        Spawnables& a = m.get_alerts();
+        Spawner s(a);
+
+        s.run();
+    }
+
+    SECTION("with persons") {
+        PersonsGenerator pg("../resources/persons.txt");
+        Spawnables& p = pg.get_persons();
+        Spawner s(p);
+
+        s.run();
+    }
 }
