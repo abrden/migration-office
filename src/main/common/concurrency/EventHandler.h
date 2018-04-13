23f9ea7d29fa1c72371555a8c12c1982248fc6ab
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-04-13 10:10:47 -0300

    Add signal and event handler

---
+++ src/main/common/concurrency/EventHandler.h
@@ -0,0 +1,11 @@
+#ifndef MIGRATION_OFFICE_EVENTHANDLER_H
+#define MIGRATION_OFFICE_EVENTHANDLER_H
+
+class EventHandler {
+
+	public:
+		virtual int handle_signal(int signum) = 0;
+		virtual ~EventHandler() {};
+};
+
+#endif /* MIGRATION_OFFICE_EVENTHANDLER_H */
