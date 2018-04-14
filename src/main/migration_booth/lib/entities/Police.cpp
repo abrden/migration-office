70450b82531be26212bd22e2d6351731829789f2
Manuel Porto <manu.porto94@gmail.com>
2018-04-14 15:28:52 -0300

    Add fugitives and share memory attributes

--- src/main/migration_booth/lib/entities/Police.cpp
+++ src/main/migration_booth/lib/entities/Police.cpp
@@ -2,6 +2,15 @@
 
 #include "Police.h"
 
+// TODO review this
+const std::string FILE_NAME = "/bin/ls";
+const char LETTER = 'A';
+
+Police::Police() : sm(FILE_NAME, LETTER) {
+    fugitives = sm.read();
+    std::cout << "I'm the prestigious Policia Bonaerense, I received " << fugitives.size() << " fugitives ids" << std::endl;
+}
+
 bool Police::is_fugitive(Resident* resident) {
     // TODO
     return false;
@@ -17,4 +26,3 @@ void Police::report(Resident* resident) {
     std::cout << "Resident " << resident->get_id() << " you are arrested" << std::endl;
 }
 
-void Police::add_fugitive(const unsigned int id) {}
--- src/main/migration_booth/lib/entities/Police.h
+++ src/main/migration_booth/lib/entities/Police.h
@@ -1,16 +1,22 @@
 #ifndef MIGRATION_OFFICE_POLICE_H
 #define MIGRATION_OFFICE_POLICE_H
 
+#include "SharedMemory.h"
 #include "Resident.h"
 #include "Foreigner.h"
 
+#include <vector>
+
 class Police {
 
+    private:
+        SharedMemory<std::vector<unsigned int>> sm;
+        std::vector<unsigned int> fugitives;
     public:
+        Police();
         bool is_fugitive(Resident* resident);
         bool is_wanted_person(Foreigner* foreigner);
         void report(Resident* resident);
-        void add_fugitive(const unsigned int id);
 
 };
 
