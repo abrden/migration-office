1ede350b9f77aafaaf2a9d93ffe7d268a9902c1e
Manuel Porto <manu.porto94@gmail.com>
2018-04-14 20:58:47 -0300

    Add custom error message

--- src/main/common/concurrency/ExclusiveLock.cpp
+++ src/main/common/concurrency/ExclusiveLock.cpp
@@ -10,7 +10,7 @@ ExclusiveLock::ExclusiveLock(const std::string& name) : Lock(name) {
     fl.l_len = 0;
     fd = open(this->name.c_str(), O_CREAT | O_WRONLY, 0777);
     if (fd == -1) {
-        throw std::system_error(errno, std::generic_category());
+        throw std::system_error(errno, std::generic_category(), "Error when trying to open file.");
     }
 }
 
@@ -18,7 +18,7 @@ int ExclusiveLock::lock() {
     fl.l_type = F_WRLCK;
     int lock_fd = fcntl(fd, F_SETLKW, &(fl));
     if (lock_fd == -1) {
-        throw std::system_error(errno, std::generic_category());
+        throw std::system_error(errno, std::generic_category(), "Error when trying to lock file.");
     }
     return lock_fd;
 }
