4be9b7a0d3eebc962ec232921f7a0ddadc555618
Manuel Porto <manu.porto94@gmail.com>
2018-04-16 11:51:43 -0300

    Comment out no longer valid tests (will delete them in before PR)

--- src/test/test_minister.cpp
+++ src/test/test_minister.cpp
@@ -1,9 +1,11 @@
-#include "MinisterOfSecurity.h"
+//#include "src/main/ministry_of_security/MinisterOfSecurity.h"
 #include "catch.hpp"
 #include <list>
 
-TEST_CASE("Minister") {
-    MinisterOfSecurity m("../resources/alerts.txt", "../resources/fugitives.txt", 1);
-    Spawnables& a = m.get_alerts();
-    REQUIRE(a.get_items().size() == 3);
-}
+//TEST_CASE("Minister") {
+//    MinisterOfSecurity m("../resources/alerts.txt", "../resources/fugitives.txt", 1);
+//    Spawnables& a = m.get_alerts();
+//    REQUIRE(a.get_items().size() == 3);
+//}
+
+// FIXME delete this test, it's no longer useful
