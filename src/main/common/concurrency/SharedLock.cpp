3ff697e1659afe41a0be746a4d4e879d8b98b279
Manuel Porto <manu.porto94@gmail.com>
2018-04-18 11:04:29 -0300

    Fix debug print message

--- src/main/common/concurrency/SharedLock.cpp
+++ src/main/common/concurrency/SharedLock.cpp
@@ -10,7 +10,7 @@ SharedLock::SharedLock(const std::string& name) : Lock(name) {
     fl.l_len = 0;
     fd = open(name.c_str(), O_CREAT | O_RDONLY, 0777);
     if (fd == -1) {
-        throw std::system_error(errno, std::generic_category(), "Error when trying to open file.");
+        throw std::system_error(errno, std::generic_category(), "Error when trying to open lock file.");
     }
 }
 
