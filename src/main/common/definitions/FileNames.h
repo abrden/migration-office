892ffa5285ccd87c034becb0c800a6280675ab9c
Manuel Porto <manu.porto94@gmail.com>
2018-04-20 11:17:29 -0300

    Do not declare constants as extern

--- src/main/common/definitions/FileNames.h
+++ src/main/common/definitions/FileNames.h
@@ -4,16 +4,16 @@
 #include <string>
 
 namespace BinaryNames {
-    extern const std::string BOOTH_BINARY = "./migration_booth";
-    extern const std::string SPAWNER_BINARY = "./migration_spawner";
-    extern const std::string MINISTER_BINARY = "./ministry_of_security";
-    extern const std::string STATISTICS_BINARY = "./statistics";
+    const std::string BOOTH_BINARY = "./migration_booth";
+    const std::string SPAWNER_BINARY = "./migration_spawner";
+    const std::string MINISTER_BINARY = "./ministry_of_security";
+    const std::string STATISTICS_BINARY = "./statistics";
 }
 
 namespace StatisticsSharedMemory {
-    extern const std::string STATS_FILE = "/bin/ls";
-    extern const std::string LOCK_STATS_FILE = "/tmp/statistics_shm_lock";
-    extern const char LETTER = 'A';
+    const std::string STATS_FILE = "/bin/ls";
+    const std::string LOCK_STATS_FILE = "/tmp/statistics_shm_lock";
+    const char LETTER = 'A';
 }
 
 #endif //MIGRATION_OFFICE_FILENAMES_H
