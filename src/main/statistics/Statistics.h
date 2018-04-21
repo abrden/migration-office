959252a435b4522c2cdc3cad158d3851a5d17724
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-04-20 21:15:05 -0300

    Privati[zsc]e some methods

--- src/main/statistics/Statistics.h
+++ src/main/statistics/Statistics.h
@@ -22,14 +22,14 @@ class Statistics {
         void initialize_data();
         void send_initialized_data_confirmation();
         StatisticsData update_data();
-
-    public:
-        explicit Statistics(size_t booths_number, const bool debug, const std::string& log_file);
-        void start();
         size_t get_allowed_residents();
         size_t get_detained_residents();
         size_t get_allowed_foreigners();
         size_t get_deported_foreigners();
+
+    public:
+        explicit Statistics(size_t booths_number, const bool debug, const std::string& log_file);
+        void start();
 
 };
 
