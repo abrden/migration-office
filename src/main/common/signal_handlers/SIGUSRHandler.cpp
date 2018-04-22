7500adeb3b5b109898ecabb717d8c38c582abccf
Manuel Porto <manu.porto94@gmail.com>
2018-04-22 18:02:49 -0300

    Create SIGUSRHandler

---
+++ src/main/common/signal_handlers/SIGUSRHandler.cpp
@@ -0,0 +1,17 @@
+#include "SIGUSRHandler.h"
+
+#include <assert.h>
+
+SIGUSRHandler::SIGUSRHandler() : news_available(0) {}
+
+SIGUSRHandler::~SIGUSRHandler() = default;
+
+int SIGUSRHandler::handle_signal(int signum) {
+    assert(signum == SIGUSR1);
+    news_available = 1;
+    return 0;
+}
+
+sig_atomic_t SIGUSRHandler::get_news_available() const {
+    return news_available;
+}
---
+++ src/main/common/signal_handlers/SIGUSRHandler.h
@@ -0,0 +1,22 @@
+#ifndef MIGRATION_OFFICE_SIGUSRHANDLER_H
+#define MIGRATION_OFFICE_SIGUSRHANDLER_H
+
+#include "EventHandler.h"
+
+#include <csignal>
+
+class SIGUSRHandler : public EventHandler {
+
+    private:
+        sig_atomic_t news_available;
+
+    public:
+        SIGUSRHandler();
+        ~SIGUSRHandler() override;
+        int handle_signal(int signum) override;
+        sig_atomic_t get_news_available() const;
+
+};
+
+
+#endif //MIGRATION_OFFICE_SIGUSRHANDLER_H
