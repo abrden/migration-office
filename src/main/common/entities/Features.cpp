e595ee465045d2e3f468b986d7f4d08eae47297c
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-04-23 14:18:00 -0300

    I forgot the actual sort :flushed:

--- src/main/common/entities/Features.cpp
+++ src/main/common/entities/Features.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Features.h"
 
 Features::Features(const std::list<std::string>& features) : features(features) {}
@@ -5,9 +6,11 @@ Features::Features(const std::list<std::string>& features) : features(features) 
 bool Features::operator==(const Features& other) const {
     std::list<std::string> features_sorted;
     std::copy(features.begin(), features.end(), std::back_inserter(features_sorted));
+    features_sorted.sort();
 
     std::list<std::string> other_features_sorted;
     std::copy(other.features.begin(), other.features.end(), std::back_inserter(other_features_sorted));
+    other_features_sorted.sort();
 
     return features_sorted == other_features_sorted;
 }
