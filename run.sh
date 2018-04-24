928c48cc0991dbc53747861cd391499683285120
Manuel Porto <manu.porto94@gmail.com>
2018-04-24 09:57:26 -0300

    Fix script return code

--- run.sh
+++ run.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
 cd build
-echo "==================== Running tests ===================="
-TEST=$(valgrind --suppressions=../vg.supp --leak-check=full\
+echo "==================== Running tests and project ===================="
+valgrind --suppressions=../vg.supp --leak-check=full\
          --show-leak-kinds=all\
          --trace-children=yes\
          --error-exitcode=1 --errors-for-leak-kinds=all\
-         ./tests)
-
-echo "==================== Done ===================="
-echo "==================== Running project ===================="
-PROJECT=$(echo "exit" | valgrind --suppressions=../vg.supp\
+         ./tests && \
+         echo "exit" | valgrind --suppressions=../vg.supp\
          --leak-check=full\
          --show-leak-kinds=all\
          --trace-children=yes\
          --error-exitcode=1 --errors-for-leak-kinds=all\
          ./migration_office -p ../resources/people.txt\
          -a ../resources/alerts.txt\
-         -f ../resources/fugitives.txt -b 4 -s 1 -d)
-
-if [[ ! -z "$TEST" ]] || [[ ! -z "$PROJECT" ]]; then
-    exit -1
-else
-    exit 0
-fi
+         -f ../resources/fugitives.txt -b 4 -s 1 -d
