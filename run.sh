446b049fdb88768142126fc93633cdd9de2f98de
Manuel Porto <manu.porto94@gmail.com>
2018-04-24 00:48:19 -0300

    Add suppression file

--- run.sh
+++ run.sh
@@ -11,14 +11,14 @@ TEST=$(valgrind --leak-check=full\
 
 echo "==================== Done ===================="
 echo "==================== Running project ===================="
-PROJECT=$(echo "exit" | valgrind --gen-suppressions=all\
+PROJECT=$(echo "exit" | valgrind --suppressions=../vg.supp\
          --leak-check=full\
          --show-leak-kinds=all\
          --trace-children=yes\
          --error-exitcode=1 --errors-for-leak-kinds=all\
          ./migration_office -p ../resources/people.txt\
          -a ../resources/alerts.txt\
-         -f ../resources/fugitives.txt -b 1 -s 1 -d)
+         -f ../resources/fugitives.txt -b 4 -s 1 -d)
 
 if [[ ! -z "$TEST" ]] || [[ ! -z "$PROJECT" ]]; then
     exit -1
