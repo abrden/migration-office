7e6773d9b087f019409082560c4e328299e9c1aa
Manuel Porto <manu.porto94@gmail.com>
2018-04-25 08:01:54 -0300

    Fix spawn

--- src/main/alert_deleter/AlertDeleter.cpp
+++ src/main/alert_deleter/AlertDeleter.cpp
@@ -1,3 +1,4 @@
+#include <src/main/common/definitions/AlertData.h>
 #include "AlertDeleter.h"
 #include "AlertData.h"
 #include "FileNames.h"
@@ -13,11 +14,17 @@ AlertDeleter::AlertDeleter(Logger& logger, const std::string& alerts_file)
 }
 
 void AlertDeleter::spawn(std::string spawnable) {
-    AlertData data = {.id = 0};
+    AlertData data;
+    data.id = 0;
 
     alerts_shmem_lock.lock();
-    size_t pos = std::hash<std::string>{}(spawnable) % BUFFSIZE;
-    alerts_shm.write(pos, data);
+    size_t id = std::hash<std::string>{}(spawnable) % BUFFSIZE;
+    size_t i = 0;
+    AlertData read_data = alerts_shm.read(i);
+    while (read_data.id != id) {
+        read_data = alerts_shm.read(i++);
+    }
+    alerts_shm.write(id, data);
     alerts_shmem_lock.unlock();
 }
 
