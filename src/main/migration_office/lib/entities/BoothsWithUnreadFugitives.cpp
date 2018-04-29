14e9e46e85e5b64323a75810a8d33ab9cb016761
Manuel Porto <manu.porto94@gmail.com>
2018-04-29 12:13:24 -0300

    Define semaphore wrapper

---
+++ src/main/migration_office/lib/entities/BoothsWithUnreadFugitives.cpp
@@ -0,0 +1,1 @@
+#include "BoothsWithUnreadFugitives.h"
---
+++ src/main/migration_office/lib/entities/BoothsWithUnreadFugitives.h
@@ -0,0 +1,12 @@
+#ifndef MIGRATION_OFFICE_BOOTHSWITHUNREADFUGITIVES_H
+#define MIGRATION_OFFICE_BOOTHSWITHUNREADFUGITIVES_H
+
+class BoothsWithUnreadFugitives {
+
+    private:
+        Semaphore booths;
+    public:
+        BoothsWithUnreadFugitives(size_t booths_number);
+};
+
+#endif //MIGRATION_OFFICE_BOOTHSWITHUNREADFUGITIVES_H
