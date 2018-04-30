cfda11b08811a7512db9d5ace11853416512c4b6
Agustina Barbetta <agustina.barbetta@gmail.com>
2018-04-30 12:47:51 -0300

    PersonsQueue logs cleanup

--- src/main/migration_booth/lib/entities/PersonsQueue.cpp
+++ src/main/migration_booth/lib/entities/PersonsQueue.cpp
@@ -8,37 +8,34 @@
 
 static const int BUFF_SIZE = 1024;
 
-enum ERRORS {
-    BUFFER_EXCEEDED = -1
-};
-
 PersonsQueue::PersonsQueue(Logger& logger) : logger(logger), fifo(PeopleFifo::FIFO_FILE), fifo_lock(PeopleFifo::BOOTH_LOCK_FILE) {}
 
 Person* PersonsQueue::front() {
     fifo_lock.lock();
     unsigned long buffer_size;
-    logger(BOOTH_QUEUE) << "Trying to read size" << std::endl;
     ssize_t bytes_read = fifo.fifo_read(static_cast<void*>(&buffer_size), sizeof(unsigned long));
-    if (bytes_read != sizeof(unsigned long)) {
-        logger(BOOTH_QUEUE) << "I couldnt read ulong. I read this many bytes: " << bytes_read << std::endl;
+    if (bytes_read == 0) {
+        logger(BOOTH_QUEUE) << "No more persons on the queue" << std::endl;
         return nullptr;
+    } else if (bytes_read != sizeof(unsigned long)) {
+        throw std::runtime_error("I couldnt read person size ulong. I read this many bytes: " + bytes_read);
     }
-    logger(BOOTH_QUEUE) << "I read size: " << buffer_size << std::endl;
 
     if (buffer_size > BUFF_SIZE) {
         throw std::overflow_error("Serialized person buffer exceeded.");
     }
     char buffer[BUFF_SIZE];
-    logger(BOOTH_QUEUE) << "Trying to read serialized person" << std::endl;
+
     bytes_read = fifo.fifo_read(static_cast<void*>(buffer), sizeof(char) * buffer_size);
-    if ((unsigned long) bytes_read != buffer_size) {
-        logger(BOOTH_QUEUE) << "I couldnt read a person. I read this many bytes: " << bytes_read << std::endl;
-        return nullptr;
+    if (bytes_read == 0) {
+        logger(BOOTH_QUEUE) << "No more persons to on the queue" << std::endl;
+    } else if ((unsigned long) bytes_read != buffer_size) {
+        throw std::runtime_error("I couldnt read a person. I read this many bytes: " + bytes_read);
     }
     fifo_lock.unlock();
 
     std::string serialized_person(buffer, buffer_size);
-    logger(BOOTH_QUEUE) << "I read person: " << serialized_person << std::endl;
+    logger(BOOTH_QUEUE) << "I received person: " << serialized_person << ". with size: " << buffer_size << std::endl;
 
     return PersonDeserializer::deserialize(serialized_person);
 }
