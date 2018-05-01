cee91a9d1cef108ca67ce503fdb3c1183943b69b
Manuel Porto <manu.porto94@gmail.com>
2018-05-01 11:30:43 -0300

    Write contents to a file

--- generator.py
+++ generator.py
@@ -2,9 +2,12 @@ import random
 import string
 
 # Fugitives
-for _ in range(100): print(random.randint(10000000, 40000000))
+with open("resources/gen_fugitives.txt", "w") as f:
+    for _ in range(20):
+        f.write(str(random.randint(10000000, 40000000)) + '\n')
 
 # Persons
+
 
 def features_generator():
     eyes_f = ["large", "small", "narrow", "sharp", "squinty"]
@@ -14,24 +17,27 @@ def features_generator():
     hair_f = ["long", "short", "bald", "messy", "shoulder-length"]
     i = random.randint(0, 4)
 
-    return eyes_f[i] + " eyes," + eyebrows_f[i] + " eyebrows," + skin_f[i] + " skin," + face_f[i] + " face," + hair_f[i] + " hair"
+    return eyes_f[i] + " eyes," + eyebrows_f[i] + " eyebrows," + skin_f[i] + " skin," + face_f[i] + " face," + \
+           hair_f[i] + " hair"
 
-round
-for _ in range(10):
-    timestamp = str(random.randint(0, 10))
-    resident = str(random.randint(0, 1))
-    res_id = str(random.randint(10000000, 40000000)) if resident == '1' else ''
-    passport = ''.join(random.choice(string.ascii_uppercase) for _ in range(3)) + \
-               ''.join(random.choice(string.digits) for _ in range(6)) if resident == '0' else ''
-    features = features_generator()
-    person = ','.join([timestamp, resident, res_id, passport, features])
-    print(person)
+
+with open("resources/gen_persons.txt", "w") as f:
+    for _ in range(20):
+        timestamp = str(random.randint(0, 10))
+        resident = str(random.randint(0, 1))
+        res_id = str(random.randint(10000000, 40000000)) if resident == '1' else ''
+        passport = ''.join(random.choice(string.ascii_uppercase) for _ in range(3)) + \
+                   ''.join(random.choice(string.digits) for _ in range(6)) if resident == '0' else ''
+        features = features_generator()
+        person = ','.join([timestamp, resident, res_id, passport, features])
+        f.write(person + '\n')
 
 # Alerts
 
-for _ in range(10):
-    timestamp = random.randint(0, 10)
-    del_timestamp = timestamp + random.randint(1, 10)
-    features = features_generator()
-    alert = ','.join([str(timestamp), str(del_timestamp), str(features)])
-    print(alert)
+with open("resources/gen_alerts.txt", "w") as f:
+    for _ in range(20):
+        timestamp = random.randint(0, 10)
+        del_timestamp = timestamp + random.randint(1, 10)
+        features = features_generator()
+        alert = ','.join([str(timestamp), str(del_timestamp), str(features)])
+        f.write(alert + '\n')
