e1a7d380c45d6f8ae04ceb770f42107966f6da1b
Manuel Porto <manu.porto94@gmail.com>
2018-05-01 11:32:32 -0300

    Write contents to a file

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ with open("resources/gen_fugitives.txt", "w") as f:
 # Persons
 
 
-def features_generator():
+def generate_features():
     eyes_f = ["large", "small", "narrow", "sharp", "squinty"]
     eyebrows_f = ["arched", "straight", "plucked", "sparse", "trim"]
     skin_f = ["dark brown", "pale", "light", "amber", "deep brown"]
@@ -21,14 +21,18 @@ def features_generator():
            hair_f[i] + " hair"
 
 
+def generate_passport():
+    return ''.join(random.choice(string.ascii_uppercase) for _ in range(3)) + \
+           ''.join(random.choice(string.digits) for _ in range(6))
+
+
 with open("resources/gen_persons.txt", "w") as f:
     for _ in range(20):
         timestamp = str(random.randint(0, 10))
         resident = str(random.randint(0, 1))
         res_id = str(random.randint(10000000, 40000000)) if resident == '1' else ''
-        passport = ''.join(random.choice(string.ascii_uppercase) for _ in range(3)) + \
-                   ''.join(random.choice(string.digits) for _ in range(6)) if resident == '0' else ''
-        features = features_generator()
+        passport = generate_passport() if resident == '0' else ''
+        features = generate_features()
         person = ','.join([timestamp, resident, res_id, passport, features])
         f.write(person + '\n')
 
@@ -38,6 +42,6 @@ with open("resources/gen_alerts.txt", "w") as f:
     for _ in range(20):
         timestamp = random.randint(0, 10)
         del_timestamp = timestamp + random.randint(1, 10)
-        features = features_generator()
+        features = generate_features()
         alert = ','.join([str(timestamp), str(del_timestamp), str(features)])
         f.write(alert + '\n')
